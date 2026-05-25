897afa068b10dcd88618b4b627f6a1b2f50c00f0
Kartik Kamatkar <KartikKamatkar20@gmail.com>
2026-05-25 20:39:26 +0530

    Floder and Structure is Created

---
+++ src/main/java/com/example/OFFUPI/OffupiApplication.java
@@ -0,0 +1,13 @@
+package com.example.OFFUPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffupiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OffupiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OFFUPI/OffupiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OFFUPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffupiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
